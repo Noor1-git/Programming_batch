a64d61139bbddec625738d197cd599c1e6f2fb68
Noor1-git <129384343+Noor1-git@users.noreply.github.com>
2023-04-03 10:49:07 +0530

    Add files via upload

---
+++ Conditional statements/Program4.java
@@ -0,0 +1,15 @@
+package sowmyamaam.day6;
+
+import java.util.Scanner;
+
+public class Program4 {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter a character");
+		char a=s.next().charAt(0);
+		int b=a;
+		s.close();
+		System.out.println(b);
+	}
+
+}
